81d2d9ca15494b46f8bc71f4bf5d52e97b3a269a
harunaabaldeh <harunaabaldeh@gmail.com>
2023-07-17 14:54:42 +0000

    vue composition api

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup></script>
+
+<template>
+  <div>
+    <router-view />
+  </div>
+</template>
+
+<style>
+#app {
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+
+body {
+  margin: 0;
+  background: #eee;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h1>Homepage</h1>
+    <input type="text" v-model="search" />
+    <p>search term - {{ search }}</p>
+    <div v-for="name in matchingNames" :key="name">{{ name }}</div>
+  </div>
+</template>
+
+<script>
+import { ref, computed, watch, watchEffect } from "vue";
+export default {
+  name: "Home",
+  setup() {
+    const search = ref("");
+
+    const names = ref([
+      "haruna",
+      "penda",
+      "awa",
+      "gundo",
+      "ishata",
+      "halimatou",
+    ]);
+
+    watch(search, () => {
+      console.log("watch ran");
+    });
+
+    watchEffect(() => {
+      console.log("watchEffect ran");
+    });
+
+    const matchingNames = computed(() => {
+      return names.value.filter((name) => name.includes(search.value));
+    });
+
+    return { names, search, matchingNames };
+  },
+};
+</script>
+
+<style></style>
